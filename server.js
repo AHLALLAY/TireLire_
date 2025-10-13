2d07172b9336d267f70c484d31ee79b291fc8dcc
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-10-13 14:37:36 +0100

    feat: Ajouter la connexion MongoDB et refonte de la page d'accueil

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dbConnection = require('./Databases/Connection');
 require('dotenv').config();
 
 
@@ -8,20 +9,25 @@ const port = process.env.PORT;
 
 
 const startServer = async () => {
-    // connexion au BDD
     try {
+        // connexion au BDD
+        await dbConnection.connectToDb();
 
+        // lacement du serveur
+        app.listen(port, () => {
+            console.log(`[Server] => http://localhost:${port}/`);
+        });
     } catch (error) {
-        
+        console.log(`[Server] => Server Error : ${error}`);
     }
-    // 
-    app.listen(port, () => {
-        console.log(`[Server Starting] => http://localhost:${port}/`);
-    });
+
 }
 
 app.get('/', (req, res)=> {
-    res.send(`<h1 style="color:green">Server is running ...</h1>`);
+    res.send(`<div style=" display: flex; justify-content: center; align-items: center; height: 100vh;
+  background: linear-gradient(135deg, #e0ffe0, #f0fff0); font-family: 'Segoe UI', sans-serif;">
+  <h1 style="color: #2e7d32; font-size: 2.5rem;"> Server is running ... </h1>
+</div>`);
 })
 
 startServer();
