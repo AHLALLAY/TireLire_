d3218261350b56dfd8d1727fee71aad88250e954
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-10-14 16:12:53 +0100

    feat: Ajouter la méthode 'login' de 'Authcontroller'

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,55 +1,99 @@
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 const User = require('../Models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-class authController{
+class AuthController {
 
-    async register(req, res){
-        try{
+    async register(req, res) {
+        try {
             const errors = validationResult(req);
-            if(!errors.isEmpty()){
+            if (!errors.isEmpty()) {
                 return res.status(400).json({ errors: errors.array() });
             }
 
-            const {firstName, lastName, email, password, phone, nationalId, nationalIdImage, selfieImage} = req.body;
+            const { firstName, lastName, email, password, phone, nationalId, nationalIdImage, selfieImage } = req.body;
 
-            const user = await User.findOne({email});
-            if(user){
-                return res.status(400).json({message: 'this email already existe'});
+            const user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ message: '[message] Email already exists' });
             }
 
             const hashedPassword = await bcrypt.hash(password, 10);
 
             const newUser = new User({
-                firstName, 
-                lastName, 
-                email, 
-                password: hashedPassword, 
-                phone, 
-                nationalId, 
-                nationalIdImage, 
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+                phone,
+                nationalId,
+                nationalIdImage,
                 selfieImage
             });
 
             await newUser.save();
 
-            return res.status(201).json({message: 'user created with success',});
+            return res.status(201).json({ message: '[Message] User created successfully', });
 
-        }catch(error){
-            console.log(`[register] => errors : ${error}`);
-            if(error.code === 11000){
+        } catch (error) {
+            console.log(`[register] => error : ${error}`);
+            if (error.code === 11000) {
                 return res.status(400).json({
-                    message: 'Email, phone, national Id already existe',
+                    message: '[MongooDb] Duplicate field value entered',
                 });
             }
-            return res.status(500).json({message: `Unexpected error : ${error}`});
+            return res.status(500).json({ message: `[server] Internal server error : ${error}` });
         }
     }
 
-    login(email, password){
+    async login(req, res) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
+            const { email, password } = req.body;
+            const user = await User.findOne({ email });
+            if (!user) {
+                res.status(404).json({ message: '[Message] User not found' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                res.status(401).json({ message: '[Message] Invalid credentials' });
+            }
+
+            const token = jwt.sign(
+                { expiresIn: '1h', },
+                process.env.JWT_SECRET,
+                {
+                    userId: user._id,
+                    email: user.email,
+                    role: user.role,
+                },
+            );
+            return res.status(200).json({
+                message: '[message] Login successful',
+                user: {
+                    id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    phone: user.phone,
+                    role: user.role,
+                    KYCStatus: user.KYCStatus,
+                }
+            });
+        } catch (error) {
+            console.log(`[login] => errors : ${error}`);
+            return res.status(500).json({
+                message: `[server] Internal server error : ${error}`
+            });
+        }
     }
 
 }
 
-module.exports = authController;
+module.exports = new AuthController();
--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-const authController = require('./../Controllers/authController');
+const authController = require('../Controllers/AuthController');
 
 routes.post('/register', authController.register);
 routes.post('/login', authController.login);
