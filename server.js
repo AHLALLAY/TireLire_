4dcf7d56fe9aa9f9730c012151d492a640cbae91
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-10-14 17:11:43 +0100

    feat: Ajout de l'initialisation automatique de l'administrateur par défaut

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const dbConnection = require('./Databases/Connection');
+const user = require('./Models/User')
 require('dotenv').config();
 
 
@@ -7,11 +9,48 @@ const app = express();
 const port = process.env.PORT;
 
 
+const InitAdmin = async () => {
+    try {
+        const adminConter = await user.countDocuments({ role: 'admin' });
+        if (adminConter === 0) {
+
+            const email = process.env.EMAIL;
+            const password = process.env.PASSWORD;
+            
+            if (!email || !password) {
+                throw new Error('Admin email or password not configured in environment variables');
+            }
+            const Hashedpassword = await bcrypt.hash(password, 10);
+
+            await user.create({
+                firstName: 'Abderrahmane',
+                lastName: 'Ahlallay',
+                email: email,
+                password: Hashedpassword,
+                phone: '0606000000',
+                nationalId: 'IE12345',
+                nationalIdImage: 'abderrahmane_ahlallay_cin.jpg',
+                selfieImage: 'abderrahmane_ahlallay_selfie.jpg',
+                role: 'admin',
+                KYCStatus: 'approved',
+            });
+
+            console.log('[Server] Default Admin Created');
+        } else {
+            console.log('[Server] Admin user already exists');
+        }
+    } catch (error) {
+        console.log(`[server] Internal server error : ${error}`);
+    }
+}
 
 const startServer = async () => {
     try {
         // connexion au BDD
         await dbConnection.connectToDb();
+
+        // Initialiser le role d'admin
+        InitAdmin();
 
         // lacement du serveur
         app.listen(port, () => {
@@ -23,7 +62,7 @@ const startServer = async () => {
 
 }
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send(`<div style=" display: flex; justify-content: center; align-items: center; height: 100vh;
   background: linear-gradient(135deg, #e0ffe0, #f0fff0); font-family: 'Segoe UI', sans-serif;">
   <h1 style="color: #2e7d32; font-size: 2.5rem;"> Server is running ... </h1>
