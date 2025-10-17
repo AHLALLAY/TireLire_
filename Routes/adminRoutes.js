9bf09439dab43f10f4fd83bb2ee8f0b98d86bd50
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-10-17 16:45:23 +0100

    fix: Connecter le contrôleur à la route KYC: - Ajouter AdminController.KYCStatsUpdater à la route PATCH - Corriger la route orpheline sans contrôleur

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const AdminController = require('../Controllers/AdminController');
 
-routes.patch('/:id/kyc-status');
+routes.patch('/:id/kyc-status', AdminController.KYCStatsUpdater);
 
 
 module.exports = routes;
