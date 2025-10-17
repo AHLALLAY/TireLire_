938b7b2acb9c96799183a2acdfc9af3f41ceafc4
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-10-17 11:55:45 +0100

    feat: Ajouter la méthode 'displayUsersById'

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -3,14 +3,14 @@ const User = require('./../Models/User');
 class UserController {
 
     async displayUsers(req, res) {
-        try{
+        try {
             const users = await User.find({});
             return res.status(200).json({
                 success: true,
                 data: users
             });
 
-        }catch(error){
+        } catch (error) {
             return res.status(500).json({
                 success: false,
                 message: `Internal Server Error: ${error}`
@@ -18,8 +18,26 @@ class UserController {
         }
     }
 
-    displayUsersById(id) {
-
+    async displayUsersById(req, res) {
+        try {
+            const user = await User.findById(req.params.id );
+            if (!user) {
+                console.log('[displayUsersById] => user not found');
+                return res.status(404).json({
+                    success: false,
+                    message: 'User Not Found'
+                });
+            }
+            return res.status(200).json({
+                success: true,
+                data: user
+            });
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: `Internal Server Error: ${error}`
+            });
+        }
     }
 }
 
