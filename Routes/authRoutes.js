8b8dea9f066c737b7da187db97c0638465b05770
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-10-19 17:58:03 +0100

    feat: Utiliser 'authMiddleware' dans 'authRoutes'

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const routes = express.Router();
 const authController = require('../Controllers/AuthController');
+const authMiddleware = require('../Middlewares/AuthMiddleware');
 
-routes.post('/register', authController.register);
-routes.post('/login', authController.login);
+routes.post(
+    '/register', 
+    authMiddleware.validateRegister, 
+    authController.register
+);
+routes.post(
+    '/login', 
+    authMiddleware.validateLogin, 
+    authController.login
+);
 
 
 module.exports = routes;
